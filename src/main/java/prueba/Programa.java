97d42ccdcc538a9e218c00bf967cba43a52b28d1
JCarlosVico <jfervic933@maralboran.es>
2021-05-08 09:47:25 +0200

    Comentarios finales

--- src/main/java/prueba/Programa.java
+++ src/main/java/prueba/Programa.java
@@ -17,13 +17,13 @@ public class Programa {
 		List<Vehiculo> listaVehiculos = findAll();
 
 		// Se imprime la lista
-		System.out.println("Todas las entidades ------------ ");
+		System.out.println("\n\nTodas las entidades ------------ ");
 		for (Vehiculo v : listaVehiculos) {
 			System.out.println(v);
 		}
 
 		// Se obtiene una entidad
-		System.out.println("Buscar Vehiculo de matrícula 0034AAB ------------ ");
+		System.out.println("\n\nBuscar Vehiculo de matrícula 0034AAB ------------ ");
 		Vehiculo aux = findByMatricula("0034AAB");
 		System.out.println(aux);
 
@@ -39,33 +39,35 @@ public class Programa {
 
 		// Se obtienen todas las instancias
 		listaVehiculos = findAll();
-		System.out.println("Todas las entidades después de crear una ------------ ");
+		System.out.println("\n\nTodas las entidades después de crear una nueva ------------ ");
 		listaVehiculos.forEach(System.out::println);
 
 		// Se modifica el precio del vehículo id = 1
 		Vehiculo vehicModificar = findByPK(1);
 		if (vehicModificar != null) {
-			vehicModificar.setPrecio(134);
+			vehicModificar.setPrecio(138);
 			modifyVehiculo(vehicModificar);
 		}
 
 		// Se obtienen todas las instancias
 		listaVehiculos = findAll();
-		System.out.println("Todas las entidades después de modificar una ------------ ");
+		System.out.println("\n\nTodas las entidades después de modificar una ------------ ");
 		listaVehiculos.forEach(System.out::println);
 
 		// Borrado del vehículo de matrícula 1235ACB
 		Vehiculo vehicBorrar = findByMatricula("1235ACB");
-		borrarVehiculo(vehicBorrar);
+		deleteVehiculo(vehicBorrar);
 
 		// Se obtienen todas las instancias
 		listaVehiculos = findAll();
-		System.out.println("Todas las entidades después de borrar una ------------ ");
+		System.out.println("\n\nTodas las entidades después de borrar una ------------ ");
 		listaVehiculos.forEach(System.out::println);
 
 	}
 
-	public static void borrarVehiculo(Vehiculo v) {
+	// Borra un registro de la tabla, aquel que tenga la misma pk que v
+	// Si no existe esa pk no hace nada
+	public static void deleteVehiculo(Vehiculo v) {
 		EntityManager em = getEntityManager();
 		Vehiculo aux = null;
 		// En este caso es necesario iniciar una transacción en la base de datos
@@ -85,23 +87,25 @@ public class Programa {
 		em.close();
 	}
 
+	// Modifica un registro de la tabla, en concreto aquel que tenga la misma pk que v
+	// Si esa pk no existe, se inserta un nuevo registro con los valores de v
 	public static void modifyVehiculo(Vehiculo v) {
 		EntityManager em = getEntityManager();
 		// En este caso es necesario iniciar una transacción en la base de datos
 		// porque vamos a persistir información en la misma
 		em.getTransaction().begin();
-		// merge(Objeto) - Si una entidad con el mismo identificador que v existe en el
+		// merge - Si una entidad con el mismo identificador que v existe en el
 		// contexto de persistencia (caché), se actualizan sus atributos y se devuelve
-		// como
-		// entidad gestionada
-		// Si el objeto v no existe en la base de datos, se comporta como persist() y la
-		// entidad gestionada es la devuelta por merge()
+		// como entidad gestionada, para guardar cambios con commit
+		// Si el objeto v no existe en la base de datos, entonces merge() se comporta 
+		// como persist()
 		em.merge(v);
 		em.getTransaction().commit();
 		em.close();
 
 	}
 
+	// Inserta un objeto en la tabla vehiculo. Si ya existe lanza una excepcion EntityExistsException
 	public static void createVehiculo(Vehiculo v) {
 		EntityManager em = getEntityManager();
 		// En este caso es necesario iniciar una transacción en la base de datos
@@ -115,27 +119,41 @@ public class Programa {
 		em.close();
 	}
 
+	// Obtiene una entidad por clave primaria, pk. Si no existe lanza 
+	// una excepción NoResultException
 	public static Vehiculo findByPK(int pk) {
 		EntityManager em = getEntityManager();
 		Vehiculo aux = null;
-		// Se crea el objeto Query a partir de una SQL nativa
+		// Se crea el objeto Query a partir de una SQL nativa, que permite pasar
+		// código SQL directamente a la base de datos
 		Query q = em.createNativeQuery("Select * from vehiculo where id = ?", Vehiculo.class);
+		// Se establece que el parámetro a usar en la query es la pk que se pasa como parámetro
+		// al método
 		q.setParameter(1, pk);
+		// Se ejecuta la consulta
 		aux = (Vehiculo) q.getSingleResult();
+		// Cierra el entityManager
+		em.close();
 		return aux;
 
 	}
 
+	// Este método obtiene todos los registros de la tabla. Si no hay registros
+	// devuelve una lista vacía
 	public static List<Vehiculo> findAll() {
 		EntityManager em = getEntityManager();
 		// Se crea la Query a partir del nombre de la NamedQuery de la clase Vehiculo
 		Query q = em.createNamedQuery("Vehiculo.findAll");
-		// Se ejecuta la consulta con getResultList haciendo un casting
+		// Se ejecuta la consulta y se hace un casting
 		List<Vehiculo> listaVehiculos = (List<Vehiculo>) q.getResultList();
+		// Cierra el entityManager
 		em.close();
 		return listaVehiculos;
 	}
 
+	// Devuelve un vehículo coincidente con la matrícula. Si no existe lanza 
+	// una excepción NoResultException
+	// Si existen varios registros con misma matrícula lanza NonUniqueResultException
 	public static Vehiculo findByMatricula(String matricula) {
 		EntityManager em = getEntityManager();
 		// Se crea la Query a partir del nombre de la NamedQuery de la clase Vehiculo
@@ -144,12 +162,14 @@ public class Programa {
 		q.setParameter("matricula", matricula);
 		// Se ejecuta la consulta
 		Vehiculo v = (Vehiculo) q.getSingleResult();
+		// Cierra el entityManager
 		em.close();
 		return v;
 	}
 
+	// Crea un objeto EntityManager usando una unidad de persistencia
 	private static EntityManager getEntityManager() {
-		// EntityManager permite realizar operaciones con la BD, lo obtenemos a
+		// EntityManager permite realizar operaciones con la BD
 		// Se obtiene a través del EntityManagerFactory y este a su vez se genera
 		// a partir del nombre de la unidad de persistencia (fichero persistence.xml)
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("rentacar");
